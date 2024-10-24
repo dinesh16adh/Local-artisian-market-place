b2e94e35abb3171d6f5ff20522e1b3056022d309
dineshadhikari01 <dinesh683adhikari@gmail.com>
2024-10-24 20:32:02 +0545

    set up

---
+++ src/assets/assets.js
@@ -0,0 +1,56 @@
+import doko from './doko.jpg'
+import art from './art.jpg'
+import khukuri from './khukuri.jpg'
+import logo from './logo.png'
+import photo from './photo.jpg'
+
+export const assets ={
+    logo,
+    doko,
+    art,
+    khukuri,
+    photo
+}
+
+export const products = [
+   {
+    _id:'aaaa',
+    name:"doko",
+    description:'this is doko usually used to carry things',
+    price:'1000',
+    Image: [doko],
+    category:'hand made',
+    date:12345678,
+    bestseller: true
+   },
+   {
+    _id:'aaaa',
+    name:"art",
+    description:'this is doko usually used to carry things',
+    price:'1000',
+    Image: [doko],
+    category:'hand made',
+    date:12345678,
+    bestseller: true
+   },
+   {
+    _id:'aaaa',
+    name:"khukuri",
+    description:'this is doko usually used to carry things',
+    price:'1000',
+    Image: [doko],
+    category:'hand made',
+    date:12345678,
+    bestseller: true
+   },
+   {
+    _id:'aaaa',
+    name:"photo",
+    description:'this is doko usually used to carry things',
+    price:'1000',
+    Image: [doko],
+    category:'hand made',
+    date:12345678,
+    bestseller: true
+   }
+]
