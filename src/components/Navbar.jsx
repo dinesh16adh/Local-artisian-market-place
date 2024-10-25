d98ddb5f9f17865eea3007b06ad42ec62686e3fd
NidinaKoirala <108610633+NidinaKoirala@users.noreply.github.com>
2024-10-25 16:21:24 +0545

    updated nav-bar to go home page when clicked on logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,11 @@ const Navbar = () => {
   
     return (
       <div className='flex items-center justify-between py-5 font-medium'>
-        <img src={assets.logo} className='w-36' alt='Logo' />   
+        {/* Wrap logo image with Link to redirect to homepage */}
+        <Link to="/">
+          <img src={assets.logo} className='w-36 cursor-pointer' alt='Logo' />
+        </Link>
         
-        {/* Updated ul with responsive classes */}
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'> 
           <NavLink to="/" className='flex flex-col items-center gap-1'>
             <p>HOME</p>
