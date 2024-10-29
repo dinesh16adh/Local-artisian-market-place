317c983b2cb25d4c1567c1682b6c712b56ce87e8
NidinaKoirala <108610633+NidinaKoirala@users.noreply.github.com>
2024-10-29 14:29:59 +0545

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { assets} from '../assets/assets';
+import { assets } from '../assets/assets';
 import { Link } from 'react-router-dom';
 import CartModal from './CartModal';
 import ProductModal from './ProductModal';
