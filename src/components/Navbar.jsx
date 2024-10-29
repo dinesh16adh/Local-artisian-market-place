d1ad835e18e9f5c47475dbc72a2148c8faf17513
NidinaKoirala <108610633+NidinaKoirala@users.noreply.github.com>
2024-10-29 16:55:51 +0545

    redirect to home after logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
             if (response.ok) {
                 localStorage.removeItem('user');
                 setIsLoggedIn(false);
-                navigate('/');  // Redirect to home after logout
+                navigate('/Local-artisian-market-place');  // Redirect to home after logout
                 window.location.reload();  // Force a full page reload to refresh the content
             } else {
                 console.error('Logout failed');
