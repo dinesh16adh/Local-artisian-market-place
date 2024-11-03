88978f1549d1ce4a8dc1a100d01fd3f47d0354db
dineshadhikari01 <dinesh683adhikari@gmail.com>
2024-11-03 22:09:51 +0545

    favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react';
+// main.jsx
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css'; // Main CSS for your app
+import App from './App.jsx'; // Main App component
 
+// Render the App component inside the #root div
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/Local-artisian-market-place">
